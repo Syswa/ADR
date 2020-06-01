a4cfaddab214aa65bed4b5e9a83499cd6814517d
Syswa <65970605+Syswa@users.noreply.github.com>
2020-06-01 23:39:31 +0200

    Projecto Final
    Versao final do projecto

--- ADR/Controllers/HomeController.cs
+++ ADR/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ADR.Models;
+using System.Net.NetworkInformation;
+using System.Text.Json.Serialization;
+using System.Net;
 
 namespace ADR.Controllers
 {
@@ -33,5 +36,56 @@ namespace ADR.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+      
+
+        [HttpPost]
+        public JsonResult HostPing(string GethostnameIP)
+        {
+          //  System.Diagnostics.Debug.WriteLine("Qual?-" + GethostnameIP);
+
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(GethostnameIP, 1000);
+            IPHostEntry entry = Dns.GetHostEntry(GethostnameIP);
+            List<HostDetails> HOST_IP_details = new List<HostDetails>();
+          
+            try
+            {
+
+
+                if (reply == null)
+                {
+                    HOST_IP_details.Add(new HostDetails() { Hostname = entry.HostName, HostIP = reply.Address.ToString(), PingTime = 0, TTL = 0, TimeNow = DateTime.Now.ToString("HH:mm:ss"), Bytes = 0 });
+
+                }
+
+                if (reply.Status == IPStatus.Success)
+                {
+                 
+                    HOST_IP_details.Add(new HostDetails() { Hostname = entry.HostName, HostIP = reply.Address.ToString(), PingTime = reply.RoundtripTime,TTL = reply.Options.Ttl, TimeNow = DateTime.Now.ToString("HH:mm:ss"), Bytes = reply.Buffer.Length });
+                }
+
+                
+            }
+            catch (PingException ex)
+            {
+
+               
+                return Json("Error Message: " + ex.Message);
+
+                HOST_IP_details.Add(new HostDetails() { Hostname = entry.HostName, HostIP = reply.Address.ToString(), PingTime = 0, TTL = 0, TimeNow = DateTime.Now.ToString("HH:mm:ss"), Bytes = 0 });
+
+
+            }
+
+
+            return Json(HOST_IP_details.ToList());
+
+
+
+        }
+
+
     }
 }
---
+++ ADR/Models/HostDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADR.Models
+{
+    public class HostDetails
+    {
+
+        public string Hostname { get; set; }
+        public string HostIP { get; set; }
+
+        public long PingTime { get; set; }
+        public int TTL { get; set; }
+
+        public string TimeNow {get;set;}
+
+        public int Bytes { get; set; }
+    }
+}
